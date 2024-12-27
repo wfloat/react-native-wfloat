56b26bcd074a411ad92b199bbfb01c673beb50cc
Mitchell Sayre <sayremitch@gmail.com>
2024-12-27 18:45:41 -0500

    Add partially set up package template for ios and android

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-native
+cd wfloat-react-native
+npm i
+
+node ./node_modules/react-native/scripts/generate-codegen-artifacts.js --targetPlatform ios --path ./example --outputPath ./generated
---
+++ ios/WfloatReactNative.mm
@@ -0,0 +1,18 @@
+#import "WfloatReactNative.h"
+
+@implementation WfloatReactNative
+
+RCT_EXPORT_MODULE()
+
+- (void)add:(double)a b:(double)b resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    NSNumber *result = [[NSNumber alloc] initWithInteger:a+b];
+    resolve(result);
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params
+{
+    return std::make_shared<facebook::react::NativeWfloatReactNativeSpecJSI>(params);
+}
+
+@end
